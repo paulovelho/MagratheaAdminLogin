42c546b1c7a96d500d0df7107e56ce92b44b7c17
paulovelho <paulovelho@paulovelho.com>
2015-07-02 11:45:47 +0100

    fixing login.php $ symbol

--- app/login.php
+++ app/login.php
@@ -1,28 +1,28 @@
 <?php
 	require("inc/global.php");
 
-	\$email = @\$_POST["email"];
-	\$pass = @\$_POST["password"];
+	$email = @$_POST["email"];
+	$pass = @$_POST["password"];
 
-	\$env = MagratheaConfig::Instance()->GetEnvironment();
+	$env = MagratheaConfig::Instance()->GetEnvironment();
 
-	\$db = new MagratheaConfigFile();
-	\$db->setPath("../database/")->setFile("adminlogin_data.magratheaDB");
-	\$db->createFileIfNotExists();
-	\$section = \$db->getConfigSection(\$env);
-	\$users = \$section["users"];
+	$db = new MagratheaConfigFile();
+	$db->setPath("../database/")->setFile("adminlogin_data.magratheaDB");
+	$db->createFileIfNotExists();
+	$section = $db->getConfigSection($env);
+	$users = $section["users"];
 
-	if(!empty(\$users)){
-		\$users = html_entity_decode(\$users);
-		\$users = json_decode(\$users);	
+	if(!empty($users)){
+		$users = html_entity_decode($users);
+		$users = json_decode($users);	
 	} 
 
-	foreach (\$users as \$u) {
-		if(\$u->email == \$email && \$u->password == \$pass)
-			\$_SESSION["user"] = \$email;
+	foreach ($users as $u) {
+		if($u->email == $email && $u->password == $pass)
+			$_SESSION["user"] = $email;
 	}
 
-	if(empty(\$_SESSION["user"]))
+	if(empty($_SESSION["user"]))
 		header("Location: admin.php?error=login_error");
 	else 
 		header("Location: admin.php");
