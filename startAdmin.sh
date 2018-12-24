77fd2023de738e6159ae07b63eec78395526eab2
Paulo Henrique Martins <paulovelho@paulovelho.com>
2018-12-24 01:42:02 -0200

    fixing admin.php

--- startAdmin.sh
+++ startAdmin.sh
@@ -136,23 +136,26 @@ echo -e "\ncreating file app/admin.php...\n"
 echo -e "\t(a new approach to an old friend)\n"
 cat <<EOF >app/admin.php
 <?php
-  include("inc/global.php");
-  include(\$magrathea_path."/MagratheaAdmin.php"); // $magrathea_path should already be declared
+	include("inc/global.php");
+	include(\$magrathea_path."/MagratheaAdmin.php"); //  should already be declared
 
-  class MagratheaLoginController extends MagratheaController {
-    public static function Login(){
-      if(@\$_GET["error"] == "login_error")
-        \$error_message = "Login or password incorrect!";
-      include "Views/login.html";
-    }
-  }
+	class LoginController extends MagratheaController {
+		public static function Login(){
+			if(@\$_GET["error"] == "login_error")
+				self::GetSmarty()->assign("message", "Login or password incorrect!");
+			else
+				self::GetSmarty()->assign("message", "");
+			self::GetSmarty()->display("login.html");
+		}
+	}
 
-  if(!empty(\$_SESSION["magrathea_user"])) {
-    \$admin = new MagratheaAdmin(); // adds the admin file
-    \$admin->Load(); // load!
-  } else {
-    MagratheaLoginController::Login();
-  }
+	if(!empty(\$_SESSION["magrathea_user"])) {
+		\$admin = new MagratheaAdmin(); // adds the admin file
+		\$admin->title = "Botecaria Admin";
+		\$admin->Load(); // load!
+	} else {
+		LoginController::Login();
+	}
 
 ?>
 EOF
